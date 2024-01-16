4e4d49b1ea79cb8382d93216cf272cfa7b9680a4
m1stadev <adamhamdi31@gmail.com>
2024-01-16 09:41:09 -0600

    module: disable logger by default

--- apple_compress/__init__.py
+++ apple_compress/__init__.py
@@ -1,4 +1,3 @@
-import sys
 from importlib.metadata import version
 
 from loguru import logger as _logger
@@ -7,10 +6,4 @@ from .compress import compress, decompress  # noqa: F401
 from .types import Algorithm  # noqa: F401
 
 __version__ = version(__package__)
-
-_logger.remove()
-_logger.add(
-    sys.stderr,
-    level='WARNING',
-    format='[{time:MMM D YYYY - hh:mm:ss A zz}] {level} {module}:{line} {message}',
-)
+_logger.disable(__package__)
